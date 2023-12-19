9731d3e5f4840e93b47d085a506cc4399b37c99f
mahavia khan <149267515+mahaviakhan@users.noreply.github.com>
2023-12-19 16:38:12 +0500

    Add files via upload
    Welcome to my first mini Python project!
    
    This simple program converts user input into speech using the Google Text-to-Speech (gTTS) library. Just type in your text, and the program will generate an MP3 file with the corresponding speech. It's a beginner-friendly project designed to demonstrate basic text-to-speech functionality in Python.
    
    Features:
    User-friendly input prompt.
    Converts text to speech using gTTS.
    Saves the generated speech as an MP3 file.
    Usage:
    Run the script.
    Enter your text when prompted.
    Type 'done' to exit the program.
    Feel free to explore, modify, and learn from this project. Happy coding!

---
+++ text to speak.py
@@ -0,0 +1,14 @@
+from gtts import gTTS
+import os
+
+if __name__=='__main__':
+    print("--welcome--")
+    while True:
+        text=input("enter a text or type 'done' to exit: ")
+        if text.lower=='done':
+            print("program to war gya!!")
+            break
+        tts=gTTS(text=text, lang='en')
+        tts.save("output.mp3")
+
+    os.system("start output.mps")
